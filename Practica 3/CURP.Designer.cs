7587a666e5025ee1fbf5087c6862512f835502f9
angelsaotz <alu.23130598@correo.itlalauna.edu.mx>
2025-02-19 14:02:52 -0600

    NUEVA RAMA CURP

---
+++ Practica 3/CURP.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Practica_3
+{
+    partial class CURP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            TbCurp = new TextBox();
+            button1 = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 20);
+            label1.TabIndex = 0;
+            label1.Text = "INGRESA TU CURP:";
+            label1.Click += label1_Click;
+            // 
+            // TbCurp
+            // 
+            TbCurp.Location = new Point(3, 39);
+            TbCurp.Name = "TbCurp";
+            TbCurp.Size = new Size(316, 27);
+            TbCurp.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(406, 39);
+            button1.Name = "button1";
+            button1.Size = new Size(191, 58);
+            button1.TabIndex = 2;
+            button1.Text = "BUSCAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = SystemColors.Highlight;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(TbCurp, 0, 1);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button1, 1, 1);
+            tableLayoutPanel1.Location = new Point(-2, -1);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 8.10810852F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 91.89189F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(807, 456);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // CURP
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "CURP";
+            Text = "CURP";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox TbCurp;
+        private Button button1;
+        private TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ Practica 3/CURP.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_3
+{
+    public partial class CURP : Form
+    {
+        public CURP()
+        {
+            InitializeComponent();
+        }
+
+        public string Estado(string estado)
+        {
+            switch (estado)
+            {
+                case "AG": return "Aguascalientes"; break;
+                case "BC": return "Baja California"; break;
+                case "BS": return "Baja California Sur"; break;
+                case "CC": return "Campeche"; break;
+                case "CS": return "Chiapas"; break;
+                case "CH": return "Chihuahua"; break;
+                case "CL": return "Coahuila"; break;
+                case "DG": return "Durango"; break;
+                case "GT": return "Guanajuato"; break;
+                case "GR": return "Guerrero"; break;
+                case "HG": return "Hidalgo"; break;
+                case "JC": return "Jalisco"; break;
+                case "MC": return "México"; break;
+                case "MN": return "Michoacán"; break;
+                case "MS": return "Morelos"; break;
+                case "NT": return "Nayarit"; break;
+                case "NL": return "Nuevo León"; break;
+                case "OC": return "Oaxaca"; break;
+                case "PL": return "Puebla"; break;
+                case "QT": return "Querétaro"; break;
+                case "QR": return "Quintana Roo"; break;
+                case "SP": return "San Luis Potosí"; break;
+                case "SL": return "Sinaloa"; break;
+                case "SR": return "Sonora"; break;
+                case "TC": return "Tabasco"; break;
+                case "TS": return "Tamaulipas"; break;
+                case "TL": return "Tlaxcala"; break;
+                case "VZ": return "Veracruz"; break;
+                case "YN": return "Yucatán"; break;
+                case "ZS": return "Zacatecas"; break;
+                default: return "Abreviatura no reconocida"; break;
+            }
+        }
+
+        public string Sexo(string sexo)
+        {
+            if (sexo == "H")
+            {
+                return "Masculino";
+            }
+            else
+            {
+                return "Femenino";
+            }
+        }
+
+        private void CURP_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string curp = TbCurp.Text;
+            string expresion = "^[A-Z]{4}\\d{6}[HM]{1}[A-Z]{2}[A-Z]{3}[0-9A-Z]{2}$";
+          
+
+            if (Regex.IsMatch(curp, expresion) == true)
+            {
+                string year = curp[4].ToString() + curp[5].ToString();
+                string mes = curp[6].ToString() + curp[7].ToString();
+                string dia = curp[8].ToString() + curp[9].ToString();
+
+                string sexo = curp[10].ToString();
+                string estado = curp[11].ToString() + curp[12].ToString();
+                DateTime fecha = DateTime.Parse(dia + "/" + mes + "/" + year);
+
+                MessageBox.Show("Tu fecha de Nacimiento es: " + fecha.ToString("dd/MM/yyyy") + "\nEl Tu sexo es: " + Sexo(sexo) + "\nEl Tu estado es: " + Estado(estado));
+            }
+            else
+            {
+                MessageBox.Show("TU CURP ES INVALIDA, ESCRIBELA CORRECTAMENTE");
+            }
+        }
+
+    }
+}
+ 
